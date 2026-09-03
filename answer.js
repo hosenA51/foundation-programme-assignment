67f2f99a2c7332037c0cb4c85d9fe35b6fd9ec70
hosenA51 <hosenahmed2552@gmail.com>
2026-09-03 12:49:03 +0600

    added answer to the question 3

--- answer.js
+++ answer.js
@@ -27,3 +27,17 @@ function getDayType(day) {
             return "Invalid Day";
     }
 }
+
+//Question No. 3
+
+function validateUsername(username) {
+    if (username.length < 4){
+        return "Too Short"
+    } else if (username.includes(" ")) {
+        return "No Space Allowed"
+    } else if (username.toLowerCase().includes("admin")){
+        return "Reserved Word"
+    } else {
+        return "Available"
+    }
+}
